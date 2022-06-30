0af3a53554c3f14778001aedc324b7f968ff5082
joaovcog <joaovictor.cogomes@gmail.com>
2022-06-30 10:49:21 -0300

    Importing lombok dependencies

--- src/main/java/com/teste/modelmapper/model/Atividade.java
+++ src/main/java/com/teste/modelmapper/model/Atividade.java
@@ -10,14 +10,14 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@SuperBuilder
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
